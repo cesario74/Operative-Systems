c27f0288562aaa24f505d61f6613fc8441442d6e
Cesário Perneta <cesarioperneta4@hotmail.com>
2018-06-03 15:22:55 +0100

    Apagados ficheiros antigos

--- makefile
+++
@@ -1,8 +0,0 @@
-OBJS = $(patsubst %.c, %.o, $(wildcard *.c))
-EXEC=notebook
-
-install:$(OBJS)
-		gcc -o $(EXEC) $(OBJS)
-
-clean:
-		rm $(EXEC) *.o
--- readline.h
+++
@@ -1,6 +0,0 @@
-#ifndef READLINE_H_   /* Include guard */
-#define READLINE_H_
-
-ssize_t readln(int fildes, void *buf, size_t nbyte);
-
-#endif // READLINE_H_
